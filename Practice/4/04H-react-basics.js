8231272dbadecbd13fdea77e4e2840260bafe19e
Yukky <egorka0303@inbox.ru>
2025-11-28 14:32:42 +0300

    Task4 practice-H

--- Practice/4/04H-react-basics.js
+++ Practice/4/04H-react-basics.js
@@ -22,10 +22,59 @@ function LoginForm() {
 	);
 }
 
+function Clock() {
+	const [currentTime, setCurrentTime] = React.useState(dayjs().format('HH:mm:ss'));
+	
+	React.useEffect(() => {
+		setInterval(() => {
+			setCurrentTime(dayjs().format('HH:mm:ss'));
+		}, 1000);
+	}, []);
+
+	return (
+		<p>Current time: { currentTime }</p>
+	);
+}
+
+function CounterApp() {
+	const [count, setCount] = React.useState(0);
+	const [autoClick, setAutoClick] = React.useState(false);
+	const countRef = React.useRef(null);
+
+	React.useEffect(() => {
+		if (!autoClick) return;
+
+		const buttonElement = countRef.current;
+
+		setInterval(() => {
+			buttonElement.click();
+		}, 1000)
+	}, [autoClick]);
+
+
+	return (
+		<>
+			<button
+				ref={countRef}
+				onClick={() => {
+					setCount((prevCount) => prevCount + 1);
+				}}
+			> {count === 1 ? `Clicked ${count} time` : `Clicked ${count} times`}</button>
+			<button onClick={() => setCount(0)}> Reset </button>
+
+			<button
+				onClick={() => setAutoClick((prev) => !prev)}
+			>{autoClick ? "Stop Auto Click" : "Start Auto Click"}</button>
+		</>
+	);
+}
+
 function App() {
 	return (
 		<>
 			<LoginForm />
+			<Clock />
+			<CounterApp />
 		</>
 	);
 	
