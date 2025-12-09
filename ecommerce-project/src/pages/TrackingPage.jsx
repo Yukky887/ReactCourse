a393995e9ac8151d1317cabc753bdc4a2f95ca56
Yukky <egorka0303@inbox.ru>
2025-12-09 22:59:48 +0300

    Generate HTML for tracking page

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,36 +1,58 @@
 import { Header } from "../components/Header";
 import "./TrackingPage.css";
-import { Link } from "react-router";
+import axios from "axios";
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router";
 
+export function TrackingPage({ cart }) {
+    const { orderId, productId } = useParams();
+    const [order, setOrder] = useState(null);
 
-export function TrackingPage() {
+    useEffect(() => {
+        const fetchTrackingData = async () => {
+            const response = await axios.get(`/api/orders/${orderId}?expand=products`);
+            setOrder(response.data);
+        }
+
+        fetchTrackingData();
+    }, [orderId]);
+
+    if (!order) {
+        return <div>Loading...</div>;
+    }
+
+    const orderProduct = order.products.find((orderProduct) => {
+        return orderProduct.productId === productId;
+    })
+
     return (
         <>
             <title>Order Tracking</title>
 
             <link rel="icon" type="image/svg+xml" href="./images/tracking-favicon.png" />
 
-            <Header />
+            <Header cart={cart} />
 
             <div className="tracking-page">
                 <div className="order-tracking">
-                    <Link className="back-to-orders-link link-primary" to="/orders">
+                    <Link className="back-to-orders-link link-primary" to={`/orders/${orderId}?expand=products`}>
                         View all orders
                     </Link>
 
                     <div className="delivery-date">
-                        Arriving on Monday, June 13
+                        Arriving on {dayjs(orderProduct.estimatedDeliveryTimeMs).format("MMMM D")}
                     </div>
 
                     <div className="product-info">
-                        Black and Gray Athletic Cotton Socks - 6 Pairs
+                        {orderProduct.product.name}
                     </div>
 
                     <div className="product-info">
-                        Quantity: 1
+                        Quantity: {orderProduct.quantity}
                     </div>
 
-                    <img className="product-image" src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+                    <img className="product-image" src={orderProduct.product.image} />
 
                     <div className="progress-labels-container">
                         <div className="progress-label">
@@ -48,6 +70,7 @@ export function TrackingPage() {
                         <div className="progress-bar"></div>
                     </div>
                 </div>
+
             </div>
         </>
     );
