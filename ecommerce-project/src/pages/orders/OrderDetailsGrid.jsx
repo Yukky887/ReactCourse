413a7f19f71f992771d3295767afb33f1733e2b5
Yukky <egorka0303@inbox.ru>
2025-12-12 14:48:46 +0300

    Make the Add to Cart button

--- ecommerce-project/src/pages/orders/OrderDetailsGrid.jsx
+++ ecommerce-project/src/pages/orders/OrderDetailsGrid.jsx
@@ -1,11 +1,22 @@
 import dayjs from "dayjs";
 import { Fragment } from 'react';
 import buyAgainIcon from '../../assets/images/icons/buy-again.png';
+import axios from "axios";
 
-export function OrderDetailsGrid({ order }) {
+export function OrderDetailsGrid({ order, loadCart }) {
+    console.log('OrderDetailsGrid props:', { order, loadCart });
     return (
         <div className="order-details-grid">
             {order.products.map((orderProduct) => {
+                const addToCart = async () => {
+                    await axios.post(`/api/cart-items`, {
+                        productId: orderProduct.product.id,
+                        quantity: 1
+                    });
+
+                    await loadCart();
+                }
+
                 return (
                     <Fragment key={orderProduct.product.id}>
                         <div className="product-image-container">
@@ -22,7 +33,8 @@ export function OrderDetailsGrid({ order }) {
                             <div className="product-quantity">
                                 Quantity: {orderProduct.quantity}
                             </div>
-                            <button className="buy-again-button button-primary">
+                            <button className="buy-again-button button-primary"
+                                onClick={addToCart}>
                                 <img className="buy-again-icon" src={buyAgainIcon} />
                                 <span className="buy-again-message">Add to Cart</span>
                             </button>
