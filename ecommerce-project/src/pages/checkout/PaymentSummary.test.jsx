16bad3924c79cbb5cb430eec7be147f13f0e597b
Yukky <egorka0303@inbox.ru>
2025-12-15 15:33:14 +0300

    add test for place order button

--- ecommerce-project/src/pages/checkout/PaymentSummary.test.jsx
+++ ecommerce-project/src/pages/checkout/PaymentSummary.test.jsx
@@ -1,11 +1,16 @@
 import { it, expect, describe, vi, beforeEach } from 'vitest';
-import { render, screen } from "@testing-library/react";
-import { MemoryRouter } from 'react-router';
+import { render, screen, within } from "@testing-library/react";
+import { MemoryRouter, useLocation } from 'react-router';
+import axios from 'axios';
+import userEvent from '@testing-library/user-event';
 import { PaymentSummary } from './PaymentSummary';
+
+vi.mock("axios");
 
 describe("PaymentSummary component", () => {
     let paymentSummary;
     let loadCart;
+    let user;
 
     beforeEach(() => {
         paymentSummary = {
@@ -17,14 +22,15 @@ describe("PaymentSummary component", () => {
         };
 
         loadCart = vi.fn();
+        user = userEvent.setup();
     });
 
     it("displays the correct details", async () => {
         render(
             <MemoryRouter>
-                <PaymentSummary 
-                    paymentSummary={paymentSummary} 
-                    loadCart={loadCart} 
+                <PaymentSummary
+                    paymentSummary={paymentSummary}
+                    loadCart={loadCart}
                 />
             </MemoryRouter>
         );
@@ -32,21 +38,44 @@ describe("PaymentSummary component", () => {
         expect(
             screen.getByTestId("payment-summary-product-cost")
         ).toHaveTextContent("$285.80");
-        
+
         expect(
             screen.getByTestId("payment-summary-shipping-cost")
         ).toHaveTextContent("$0.00");
-        
+
         expect(
             screen.getByTestId("payment-summary-before-tax")
         ).toHaveTextContent("$285.80");
-        
+
         expect(
             screen.getByTestId("payment-summary-tax")
         ).toHaveTextContent("$28.58");
-        
+
         expect(
             screen.getByTestId("payment-summary-total")
         ).toHaveTextContent("$314.38");
+    });
+
+    it("places an order", async () => {
+        function Location() {
+            const location = useLocation();
+            return <div data-testid="url-path">{location.pathname}</div>;
+        }
+
+        render(
+            <MemoryRouter>
+                <PaymentSummary
+                    paymentSummary={paymentSummary}
+                    loadCart={loadCart}
+                />
+                <Location />
+            </MemoryRouter>
+        );
+        const placeOrderButton = screen.getByTestId("place-order-button");
+        await user.click(placeOrderButton);
+
+        expect(axios.post).toHaveBeenCalledWith('api/orders');
+        expect(loadCart).toHaveBeenCalled();
+        expect(screen.getByTestId('url-path')).toHaveTextContent('/orders');
     });
 });
